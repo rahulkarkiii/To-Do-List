7bbb23f2d53666cf1c31010ef5f707dc6965490c
rahulkarkiii <rahulkarkeee@gmail.com>
2026-08-10 13:10:48 +0545

    complete registration

---
+++ Backend/accounts/serializer.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        min_length = 8)
+    password2 = serializers.CharField(
+        write_only=True,
+    )
+    class Meta:
+        model = User
+        fields =[
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'password',
+            'password2',
+        ]
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError('Username already exists.')
+
+        return value
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError('Email already exists.')
+        return value
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('Passwords do not match.')
+        return data
+    def create(self, validated_data):
+        validated_data.pop('password2')
+
+        user= User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            password=validated_data['password'],
+        )
+        return user
---
+++ Backend/accounts/views.py
@@ -0,0 +1,27 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializer import RegisterSerializer
+
+class RegisterView(APIView):
+    def post(self,request):
+        serializer =RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(
+                {'message': 'Registration Successful.',
+                 'user': {
+                     'id': user.id,
+                     'username': user.username,
+                     'email': user.email,
+                     'first_name': user.first_name,
+                     'last_name': user.last_name,
+                 }
+                 },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
