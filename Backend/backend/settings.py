4396e9fdf3a6d0637cfd568780280bbf12b55683
rahulkarkiii <rahulkarkeee@gmail.com>
2026-08-13 22:51:36 +0545

    Update settings

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'accounts',
+    'tasks',
+    'corsheaders',
+
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
